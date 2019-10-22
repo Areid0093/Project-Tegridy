eba76af92106cbfdc7e32db8baab171b595e612a
Alec Reid <alecreid0093@gmail.com>
2019-10-22 09:54:31 -0500

    #2

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,5 @@ class User < ApplicationRecord
     has_many :job_applications
     has_many :jobs, through: :job_applications
     has_many :bookmarks
+    validates :username, presence: true, uniqueness: true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
   resources :job_applications
   resources :job_postings
   resources :users
+  resources :sessions, only: [:new, :create, :destroy]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
