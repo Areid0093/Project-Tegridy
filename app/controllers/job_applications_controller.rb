ab9a215fdadd08c391888ea473da0e23a7220882
Alec Reid <alecreid0093@gmail.com>
2019-10-23 09:49:50 -0500

    Day3

--- app/controllers/job_applications_controller.rb
+++ app/controllers/job_applications_controller.rb
@@ -4,12 +4,25 @@ class JobApplicationsController < ApplicationController
         @job_application = JobApplication.find(params[:id])
     end
 
+    def index 
+        @job_applications = JobApplication.all 
+    end
+
     def new 
         @job_application = JobApplication.new
     end
     
     def edit
         @job_application = JobApplication.find(params[:id])
+    end
+
+    def create 
+        @job_application = JobApplication.new(job_application_params)
+        if @job_application.save
+            redirect_to @job_applications
+        else
+            render :new
+        end
     end
 
     def update
@@ -21,19 +34,11 @@ class JobApplicationsController < ApplicationController
         end
     end
 
-    def create 
-        @job_application = JobApplication.new(job_application_params)
-        if @job_application.save
-            redirect_to job_application_path(@job_application)
-        else
-            render :new
-        end
-    end
 
     private
 
     def job_application_params
-        params.require(:job_application).permit(:status, :user_id, :job_id)
+        params.require(:job_application).permit(:status, :user_id, :job_posting_id, :description)
     end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   resources :bookmarks
   resources :filters
   resources :job_applications
-  resources :job_postings
+  resources :job_postings, only: [:index, :show]
   resources :users
   resources :sessions, only: [:new, :create, :destroy]
   get "signup", to: "users#new", as: "signup"
