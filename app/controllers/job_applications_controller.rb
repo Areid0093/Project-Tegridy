cac2dc1ebf6b5dcdf010b57a64c11a4f3e36621e
a4alaniz <a4alaniz@gmail.com>
2019-10-23 14:49:33 -0500

    damn

--- app/controllers/job_applications_controller.rb
+++ app/controllers/job_applications_controller.rb
@@ -1,12 +1,11 @@
 class JobApplicationsController < ApplicationController
-
-   
-
+    before_action :find_job_application, only: [:show, :edit, :update, :destroy]
     def show
         @job_application = JobApplication.find(params[:id])
     end
     def index 
-        @job_applications = JobApplication.all
+        @job_applications = JobApplication.where("user_id = ?", session[:user_id])
+        @user = session[:user_id]
     end
     def new 
         @job_application = JobApplication.new
@@ -16,14 +15,17 @@ class JobApplicationsController < ApplicationController
         @job_application = JobApplication.find(params[:id])
     end
 
-    def create 
-        @job_application = JobApplication.new(job_application_params)
-        if @job_application.save
-            redirect_to job_application_path(@job_application)
-        else
-            render :new
-        end
-    end
+
+        def create
+            job_posting_id = job_application_params(:job_posting_id)[:job_posting_id]
+            @job_application = JobApplication.create(
+              status: "started", 
+              user_id: session[:user_id], 
+              job_posting_id: job_posting_id,
+              description: params[:job_application][:description])
+            redirect_to job_applications_path
+          end
+
 
     def update
         @job_application = JobApplication.find(params[:id])
@@ -36,7 +38,18 @@ class JobApplicationsController < ApplicationController
 
     private
 
-    def job_application_params
+    def find_job_application
+        @job_application = JobApplication.find_by(id: params[:id])
+        if @job_application.nil?
+          flash[:notice] = "Sorry, incorrect URL"
+          redirect_to login_url
+        elsif @job_application.user_id != session[:user_id]
+          flash[:notice] = "You cannot access this page"
+          redirect_to login_url
+        end
+      end
+
+    def job_application_params(*args)
         params.require(:job_application).permit(:status, :user_id, :job_posting_id, :description)
     end
 
